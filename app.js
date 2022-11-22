0281cd035b530df8fa96152553f7a580f60e6ec7
Gauthier <dmitrygauthier@gmail.com>
2022-11-22 14:31:22 +0000

    refactoring

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
+const _ge = (elem) => document.querySelector(elem);
+
 const appHeight = () => {
     const doc = document.documentElement;
     doc.style.setProperty('--app-height', `${window.innerHeight}px`);
 };
 
+const currency = {
+    'usd': 'United States Dollar',
+    'eur': 'Euro',
+    'rub': 'Russian Ruble',
+    'krw': 'South Korean Won'
+};
+
+const selectedCurrency = Object.keys(currency)[0];
+
+const renderCard = (data) => {
+    const coins = Object.keys(data);
+
+    for (let coin of coins) {
+        const coinInfo = data[coin];
+        const price = coinInfo[currency].toFixed(2);
+        const change = coinInfo[`${currency}_24h_change`].toFixed(2);
+
+        priceContainer.innerText = `$${price}`;
+
+        changeContainer.style.color = `${change < 0 ? 'red' : 'green'}`;
+        changeContainer.innerHTML = `${
+            change < 0 ? change : '+' + change
+        }% <span>&bull;</span> 24h`;
+    }
+}
+
 const getPrice = () => {
-    const currency = ['usd'];
-    const ids = ['the-open-network'];
-
-    const priceContainer = document.querySelector('[data-card="price"]');
-    const changeContainer = document.querySelector('[data-card="change"]');
-
     fetch(
-        `https://api.coingecko.com/api/v3/simple/price?ids=${ids.join(
-            ','
-        )}&vs_currencies=${currency.join(',')}&include_24hr_change=true`
-    )
-        .then((result) => result.json())
-        .then((data) => {
-            const coins = Object.getOwnPropertyNames(data);
-
-            for (let coin of coins) {
-                const coinInfo = data[coin];
-                const price = coinInfo[currency].toFixed(2);
-                const change = coinInfo[`${currency}_24h_change`].toFixed(2);
-
-                priceContainer.innerText = `$${price}`;
-
-                changeContainer.style.color = `${change < 0 ? 'red' : 'green'}`;
-                changeContainer.innerHTML = `${
-                    change < 0 ? change : '+' + change
-                }% <span>&bull;</span> 24h`;
-            }
-        });
+        `https://api.coingecko.com/api/v3/simple/price?ids=the-open-network&vs_currencies=${selectedCurrency}&include_24hr_change=true`
+    ).then((result) => result.json())
+    .then(renderCard);
 
     setTimeout(getPrice, 20000);
 };
@@ -39,4 +43,10 @@ const getPrice = () => {
 window.addEventListener('resize', appHeight);
 appHeight();
 
-window.addEventListener('load', getPrice);
+window.addEventListener('load', () => {
+    getPrice();
+});
+
+// DOM
+const priceContainer = _ge('[data-card="price"]');
+const changeContainer = _ge('[data-card="change"]');
