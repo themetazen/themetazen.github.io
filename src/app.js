50c69f8be72e95a5b70bd703226e9d9d77345a6b
Gauthier <dmitrygauthier@gmail.com>
2023-06-12 09:14:27 +0300

    fixbugs, added visual functional and refactoring code

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-import { get } from './utils/api.service';
 import Select from './components/select';
+import { createElement } from './utils/createElement';
+import { ButtonComponent } from './components/button';
 
 import './style/main.scss';
 
-const host = 'https://api.coingecko.com/api/v3'; 
+const host = 'https://api.coingecko.com/api/v3';
 const actions = {
     coin: '/coins/markets',
 };
@@ -13,6 +14,7 @@ const currency = {
     usd: {name: 'United States Dollar', symbol: '$'},
     eur: {name: 'Euro', symbol: '€'},
     rub: {name: 'Russian Ruble', symbol: '₽'},
+    aed: {name: 'UAE Dirham', symbol: 'DH'},
     krw: {name: 'South Korean Won', symbol: '₩'},
     cny: {name: 'China Yuan', symbol: '¥'},
 };
@@ -20,9 +22,12 @@ const currency = {
 const defaultStorage = {
     currency: 'usd',
     address: null,
+    lastPrice: 0.00,
+    lastChange: 0.00
 };
 
 const elem = (sel) => document.querySelector(sel);
+
 const fixHeight = () => {
     const doc = document.documentElement;
     doc.style.setProperty('--app-height', `${window.innerHeight}px`);
@@ -39,29 +44,46 @@ const renderCard = (data) => {
     const change = price_change_percentage_24h.toFixed(2);
     const symbol = currency[storage.currency].symbol;
 
-    cardContainer.innerHTML = `
+    const card = `
         <div class="card__price">${symbol}${price}</div>
         <div class="card__change ${change < 0 ? 'falling' : 'rising'}"}">
             <strong>${change}% <span>&bull;</span> 24h<strong>
         </div>
     `;
+    cardContainer.innerHTML = card;
 };
 
-const fetchCoin = async () => {
-    try {
-        const response = await get(`${host}${actions.coin}?vs_currency=${storage.currency}&ids=the-open-network`);
-        renderCard(response);
-    } catch (error) {
-        console.error(error.message);
+const renderLoader = () => {
+    let loader = `<div class="loader"></div>`;
+    cardContainer.innerHTML = loader;
+}
+
+const fetchCoin = async (isInit = false) => {
+    if(isInit) {
+        renderLoader();
     }
-    setTimeout(fetchCoin, 20000);
+
+    try {
+        const response = await fetch(`${host}${actions.coin}?vs_currency=${storage.currency}&ids=the-open-network`);
+
+        if (!response.ok) {
+            throw new Error(await response.text());
+        }
+
+        const data = await response.json();
+
+        renderCard(data);
+    } catch (error) {
+        throw error;
+    }
+
+    setTimeout(fetchCoin, 60000);
 };
 
 const changeCurrencyHandler = (item) => {
     storage.currency = item.name;
     localStorage.setItem(`${GLOBAL_STORAGE_KEY}`, JSON.stringify(storage));
-    cardContainer.innerHTML = `<div class="loader"></div>`;
-    fetchCoin();
+    fetchCoin(true);
 };
 
 const slidebarToggleHandler = () => {
@@ -74,20 +96,20 @@ window.addEventListener('resize', fixHeight);
 fixHeight();
 
 window.addEventListener('load', () => {
-    fetchCoin();
+    fetchCoin(true);
 });
 
 const cardContainer = elem('[data-toncoin-card]');
 const slidebarToggle = elem("#slidebar .slidebar__toggle");
 const slidebarBody = elem("#slidebar .slidebar__body");
-const connectBtn = elem("#connect-btn");
+const accountContainer = elem("[data-account]");
 
 const dataSelectCurrency = Object.entries(currency).map((cur) => ({
     name: cur[0],
     label: cur[1].name
 }));
 
-new Select('#select-currency', {
+new Select('[data-select-currency]', {
     data: dataSelectCurrency,
     valueDefault: storage.currency,
     onChange(item) {
@@ -97,6 +119,20 @@ new Select('#select-currency', {
 });
 
 slidebarToggle.addEventListener('click', slidebarToggleHandler);
-connectBtn.addEventListener('click', () => {
-    alert("connect wallet");
-})
+
+accountContainer.append(
+    ButtonComponent({
+        disabled: true,
+        text: 'Connect Wallet',
+        onClick: () => alert('auth')
+    })
+);
+
+// test createElement
+// const array = ['hello', 'world', 'hey'];
+// const boolTest = true;
+// accountContainer.append(createElement('ul', {},
+//     array.map((a) => createElement('li', {style: {color: '#fff', backgroundColor: 'green'}}, a)),
+//     (boolTest ? createElement('li', {}, 'test true') : ''),
+//     createElement('li', {}, 'test')
+// ));
